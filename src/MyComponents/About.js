ec615506646ffe65147e444d8ca6edc317344ed3
Geeta Negi <geetanegi550@gmail.com>
2023-04-09 14:01:32 +0530

    complete

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an about components
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius numquam
+        quibusdam magni quaerat maiores alias, sequi dolor et nisi ut tenetur
+        voluptatem pariatur, molestias aspernatur harum ex explicabo? Saepe,
+        reiciendis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
